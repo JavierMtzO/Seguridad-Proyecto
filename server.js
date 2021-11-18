36688e32df3a0302e4c32f79a511813b0e145daf
JavierMtzO <67331708+JavierMtzO@users.noreply.github.com>
2021-11-18 01:28:43 -0600

    Aplicacion 2FA

--- server.js
+++ server.js
@@ -28,23 +28,16 @@ if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.
 }
 
 app.use(auth(config));
-
-// Middleware to make the `user` object available for all views
 app.use(function (req, res, next) {
   res.locals.user = req.oidc.user;
   next();
 });
-
 app.use('/', router);
-
-// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-// Error handlers
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
